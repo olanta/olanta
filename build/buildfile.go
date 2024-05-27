0739871e1618483bd0c48106fddd5b53d4c21069
Vitor Lobo <lobocode@gmail.com>
2024-05-27 18:14:52 -0300

    add mage mg

--- build/buildfile.go
+++ build/buildfile.go
@@ -2,34 +2,25 @@ package main
 
 import (
     "fmt"
+    "os"
+
+    "github.com/magefile/mage/mg"
     "github.com/magefile/mage/sh"
 )
 
-// Build compiles the project.
 func Build() error {
     fmt.Println("Building the project...")
-    return sh.RunV("go", "build", "-o", "bin/olanta", "./cmd/olanta")
+    return sh.RunV("go", "build", "-o", "olanta", "./cmd/olanta")
 }
 
-// Test runs the tests for the project.
 func Test() error {
     fmt.Println("Running tests...")
     return sh.RunV("go", "test", "./...")
 }
 
-// Clean removes the binary and other build artifacts.
 func Clean() error {
     fmt.Println("Cleaning build artifacts...")
-    return sh.Rm("bin/olanta")
+    return os.RemoveAll("olanta")
 }
 
-// GetCurrentDate prints the current date.
-func GetCurrentDate() error {
-    date, err := sh.Output("date", "+%a %b %d %H:%M:%S %Y")
-    if err != nil {
-        return err
-    }
-
-    fmt.Printf("::set-output name=date::%s\n", date)
-    return nil
-}
+var Default = Build
