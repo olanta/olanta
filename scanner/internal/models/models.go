9b3d2931d1481d9acb8397fb701300698f55ed32
Vitor Lobo Ramos <lobocode@gmail.com>
2024-05-28 12:27:48 -0300

    simple types

--- scanner/internal/models/models.go
+++ scanner/internal/models/models.go
@@ -1,19 +1,9 @@
 package models
 
 type Issue struct {
-    Description string `json:"description"`
-    Severity    string `json:"severity"`
-    File        string `json:"file"`
-    Line        int    `json:"line"`
-    Column      int    `json:"column"`
-}
-
-type Rule struct {
-    Pattern     string `yaml:"pattern"`
-    Description string `yaml:"description"`
-    Severity    string `yaml:"severity"`
-}
-
-type RulesConfig struct {
-    Rules []Rule `yaml:"rules"`
+	Description string
+	Severity    string
+	File        string
+	Line        int
+	Column      int
 }
