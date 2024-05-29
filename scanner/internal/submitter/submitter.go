eaee999828c4aaf8ba4a0a1dab46b7af7118c006
Vitor Lobo Ramos <lobocode@gmail.com>
2024-05-29 07:38:12 -0300

    add submitIssuesGRPC func

--- scanner/internal/submitter/submitter.go
+++ scanner/internal/submitter/submitter.go
@@ -3,6 +3,8 @@ package submitter
 import (
 	"context"
 	"fmt"
+	"log"
+	"time"
 
 	"github.com/olanta/olanta/scanner/internal/models"
 	"github.com/olanta/olanta/scanner/proto"
@@ -10,49 +12,56 @@ import (
 )
 
 func SubmitOrPrintIssues(coreURL string, issues []models.Issue) {
-	err := submitIssuesGRPC(coreURL, issues)
+	// Conectar ao servidor gRPC
+	conn, err := grpc.Dial(coreURL, grpc.WithInsecure(), grpc.WithBlock(), grpc.WithTimeout(5*time.Second))
 	if err != nil {
-		fmt.Printf("Error submitting issues: %v\n", err)
+		log.Printf("Error connecting to core server: %v", err)
 		printIssues(issues)
-	}
-}
-
-func submitIssuesGRPC(url string, issues []models.Issue) error {
-	conn, err := grpc.Dial(url, grpc.WithInsecure())
-	if err != nil {
-		return err
+		return
 	}
 	defer conn.Close()
 
 	client := proto.NewScannerServiceClient(conn)
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
 
 	protoIssues := convertToProtoIssues(issues)
-	_, err = client.Scan(context.Background(), &proto.ScanRequest{
+	_, err = client.Scan(ctx, &proto.ScanRequest{
 		Language: "java",
 		Path:     "path",
 		Issues:   protoIssues,
 	})
-	return err
-}
-
-func printIssues(issues []models.Issue) {
-	fmt.Println("Printing issues:")
-	for _, issue := range issues {
-		fmt.Printf("\nDescription: %s\nSeverity: %s\nFile: %s\nLine: %d, Column: %d\n----\n",
-			issue.Description, issue.Severity, issue.File, issue.Line, issue.Column)
+	if err != nil {
+		log.Printf("Error submitting issues: %v", err)
+		printIssues(issues)
+		return
 	}
+
+	fmt.Println("Issues submitted successfully")
 }
 
 func convertToProtoIssues(issues []models.Issue) []*proto.Issue {
 	var protoIssues []*proto.Issue
 	for _, issue := range issues {
-		protoIssues = append(protoIssues, &proto.Issue{
+		protoIssue := &proto.Issue{
 			Description: issue.Description,
 			Severity:    issue.Severity,
 			File:        issue.File,
 			Line:        int32(issue.Line),
 			Column:      int32(issue.Column),
-		})
+		}
+		protoIssues = append(protoIssues, protoIssue)
 	}
 	return protoIssues
+}
+
+func printIssues(issues []models.Issue) {
+	fmt.Println("Printing issues:")
+	for _, issue := range issues {
+		fmt.Printf("Description: %s\n", issue.Description)
+		fmt.Printf("Severity: %s\n", issue.Severity)
+		fmt.Printf("File: %s\n", issue.File)
+		fmt.Printf("Line: %d, Column: %d\n", issue.Line, issue.Column)
+		fmt.Println("----")
+	}
 }
