fe20d9f60c11940ea8288144a170f55433e2fa73
Vitor Lobo Ramos <lobocode@gmail.com>
2024-05-29 07:54:22 -0300

    add more details with grpc server

--- cmd/olanta/main.go
+++ cmd/olanta/main.go
@@ -1,57 +1,27 @@
 package main
 
 import (
-    "fmt"
-    "os"
+	"log"
+	"net"
 
-    "github.com/spf13/cobra"
-    "github.com/olanta/olanta/internal/indexer"
-    "github.com/olanta/olanta/internal/scanner"
-    "github.com/olanta/olanta/internal/helpers"
+	"github.com/olanta/olanta/internal/server"
+	"github.com/olanta/olanta/proto"
+	"google.golang.org/grpc"
 )
 
 func main() {
-    rootCmd := &cobra.Command{
-        Use:   "olanta",
-        Short: "Olanta is a static code analysis tool to find code smells and bugs.",
-    }
+	lis, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		log.Fatalf("Failed to listen: %v", err)
+	}
 
-    scanCmd := &cobra.Command{
-        Use:   "scan [language] [path]",
-        Short: "Scan the specified path for code smells and bugs.",
-        Args:  cobra.ExactArgs(2),
-        Run: func(cmd *cobra.Command, args []string) {
-            language := args[0]
-            path := args[1]
+	grpcServer := grpc.NewServer()
+	server := server.NewServer()
 
-            index, err := indexer.NewIndexer(indexer.IndexPath)
-            if err != nil {
-                helpers.Logger.Errorf("Error creating indexer: %v", err)
-                os.Exit(1)
-            }
+	proto.RegisterScannerServiceServer(grpcServer, server)
 
-            var scan scanner.Scanner
-
-            switch language {
-            case "java":
-                scan = scanner.NewJavaScanner(index)
-            case "python":
-                scan = scanner.NewPythonScanner(index)
-            default:
-                fmt.Printf("Unsupported language: %s\n", language)
-                os.Exit(1)
-            }
-
-            issues := scan.Scan(path)
-            for _, issue := range issues {
-                fmt.Printf("%+v\n", issue)
-            }
-        },
-    }
-
-    rootCmd.AddCommand(scanCmd)
-    if err := rootCmd.Execute(); err != nil {
-        fmt.Println(err)
-        os.Exit(1)
-    }
+	log.Println("Server is running on port 8080")
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("Failed to serve: %v", err)
+	}
 }
