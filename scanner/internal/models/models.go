ffc40e3676d83605d622f4d89430cfa5ca545ae5
Vitor Lobo Ramos <lobocode@gmail.com>
2024-05-30 10:29:17 -0300

    move scanner

--- scanner/internal/models/models.go
+++
@@ -1,9 +0,0 @@
-package models
-
-type Issue struct {
-	Description string
-	Severity    string
-	File        string
-	Line        int
-	Column      int
-}
--- scanner/internal/scanner/scanner.go
+++
@@ -1,7 +0,0 @@
-package scanner
-
-import "github.com/olanta/olanta/scanner/internal/models"
-
-type Scanner interface {
-    Scan(path string) []models.Issue
-}
